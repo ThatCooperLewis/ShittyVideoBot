81415eafc0fd6395b18adfc5cb1b8e009c4b8530
Cooper Lewis <thatcooperlewis@gmail.com>
2020-11-01 17:59:26 -0500

    add logging

--- main.py
+++ main.py
@@ -76,11 +76,13 @@ def main():
                     print(text)
                     for good_url in ['youtube.com', 'youtu.be', 't.co']:
                         if good_url in text:
+                            print("got good url")
                             try:
                                 # Clean tweet with secret sauce
                                 url = sanitize_url(text, good_url)
                                 # Video must meet certain content criteria
                                 if video.is_valid(url):
+                                    print("video is valid")
                                     video.playlist.add_to_queue(url)
                                     if not video.is_playing():
                                         video.play_next_if_ready()
